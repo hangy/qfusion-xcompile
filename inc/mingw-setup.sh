050e91674f09fcaa7669bd70b0b15518976b1414
Victor Luchitz <vluchits@gmail.com>
2013-09-14 23:40:09 +0200

    Switch to curl from wget

--- inc/mingw-setup.sh
+++ inc/mingw-setup.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
-./mingw-w64-setup.sh &
-./mingw-w32-setup.sh &
+sudo apt-get install curl
+
+./mingw-w64-setup.sh
+./mingw-w32-setup.sh
--- inc/mingw-w32-setup.sh
+++ inc/mingw-w32-setup.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-wget -O mingw-w32.tar.bz2 http://downloads.sourceforge.net/project/mingw-w64/Toolchains%20targetting%20Win32/Automated%20Builds/mingw-w32-bin_x86_64-linux_20130622.tar.bz2?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Fmingw-w64%2Ffiles%2FToolchains%2520targetting%2520Win32%2FAutomated%2520Builds%2F&ts=1379156712&use_mirror=optimate
-sudo mkdir -p /opt/mingw-w32-bin_x86_64-linux_20130622
-sudo tar xjf mingw-w32.tar.bz2 -C /opt/mingw-w32-bin_x86_64-linux_20130622
-sudo ln -s /opt/mingw-w32-bin_x86_64-linux_20130622 /opt/mingw-w32 && rm mingw-w32.tar.bz2
+sudo mkdir -p /opt/mingw-w32
+curl -L "http://downloads.sourceforge.net/project/mingw-w64/Toolchains%20targetting%20Win32/Automated%20Builds/mingw-w32-bin_x86_64-linux_20130622.tar.bz2?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Fmingw-w64%2Ffiles%2FToolchains%2520targetting%2520Win32%2FAutomated%2520Builds%2F&ts=1379156712&use_mirror=optimate" -o - | \
+    sudo tar -xjf -C /opt/mingw-w32 -
