652505e5c2fc95e29651f8497ddc695d74e54e29
viciious <vluchits@gmail.com>
2013-09-15 06:14:54 -0700

    Update gcc-setup.sh

--- inc/gcc-setup.sh
+++ inc/gcc-setup.sh
@@ -9,7 +9,7 @@ sudo apt-get -y install ia32-libs
 mkdir -p deb/temp
 cd deb
 
-sudo apt-get download libx11-dev:i386 libxxf86dga-dev:i386 libxext6:i386 libxext-dev:i386 libxxf86dga1:i386 libxxf86dga-dev:i386 libxxf86vm-dev:i386 libxinerama-dev:i386 libxrender-dev:i386 libxrandr-dev:i386 libsdl1.2debian:i386 libsdl1.2-dev:i386
+sudo apt-get download libx11-dev:i386 libxxf86dga-dev:i386 libxext6:i386 libxext-dev:i386 libxxf86dga1:i386 libxxf86dga-dev:i386 libxxf86vm-dev:i386 libxinerama-dev:i386 libxrender-dev:i386 libxrandr-dev:i386 libsdl1.2debian:i386 libsdl1.2-dev:i386 libopenal-dev:i386
 
 for f in *.deb; do dpkg-deb -x $f temp ; done
 sudo cp --preserve=all -d -n * ./temp/usr/lib/* /usr/lib/i386-linux-gnu/
